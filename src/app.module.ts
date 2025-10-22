7cf4af29f7b9e5ff21c7428ea3964a836f17ab47
Chinweike99 <innocentakwolu1@gmail.com>
2025-10-22 02:59:18 +0100

    fix: 404 / error

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { StringAnalysisController } from './string-analysis/controllers/string-analysis.controller';
 import { StringAnalysisService } from './string-analysis/services/string-analysis.service';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [StringAnalysisController],
-  providers: [StringAnalysisService],
+  controllers: [AppController, StringAnalysisController],
+  providers: [ AppService, StringAnalysisService],
 })
 export class AppModule {}
